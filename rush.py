45df861d985cbb83d812ea6ccc873d56d22c70eb
Stephane Gagnon <admin@pacificweb.ca>
2017-03-20 19:50:50 -0400

    Evolution

--- rush.py
+++ rush.py
@@ -1,39 +1,47 @@
 from mididings import *
 from mididings.extra import *
+from mididings.engine import *
 
 config(
 
     client_name = 'Master',
 
     out_ports = [ 
-        ('q49', '20:0','.*SD-90 Part A'),
-        ('pk5', '20:0','.*SD-90 Part A') ],
+        ('Q49', '20:0','.*SD-90 Part A'),
+        ('PK5', '20:0','.*SD-90 Part A') ],
 
-    in_ports = [ ('MidiDings IN 1', '20:2','.*SD-90 MIDI 1') ],
-
+    in_ports = [ 
+        ('MidiDings IN 1', '20:2','.*SD-90 MIDI 1'),
+        ('MidiDings IN 2', '20:3','.*SD-90 MIDI 2') ],
 )
 
+# Scene navigation
+def NavigateToScene(ev):
+    switch_scene(ev.ctrl)
+
+# Pre/Post
 _pre = Print('input', portnames='in')
 _post = Print('output', portnames='out')
 
-_control = Filter(NOTEON) >> (KeyFilter('C1') % (SceneSwitch(offset=1), SceneSwitch(offset=-1)))
+# Controller pour le changement de scene
+_control = Filter(CTRL) >> CtrlFilter([20,21,22]) >> Process(NavigateToScene)
 
-patch = Output('q49', channel=1, program=22, volume=100)
+_intro = Output('Q49', channel=1, program=22, volume=100)
 
-# RUSH
-barchetta = Velocity(fixed=80) >> Output('pk5', channel=1, program=82, volume=100, ctrls={93:75, 91:75})
-freewill = Transpose(12) >> Velocity(fixed=80) >> Output('pk5', channel=1, program=82, volume=100, ctrls={93:75, 91:75})
-#barchetta = LatchNotes(False,reset='C3') >> Velocity(fixed=80) >> Output('pk5', channel=1, program=82, volume=100, ctrls={93:75, 91:75})
-#freewill = Transpose(12) >> LatchNotes(False,reset='E4') >> Velocity(fixed=80) >> Output('pk5', channel=1, program=82, volume=100, ctrls={93:75, 91:75})
+# Patch Synth. generique pour Barchetta, FreeWill, Limelight etc...
+keysynth =  Velocity(fixed=80) >> Output('PK5', channel=1, program=82, volume=100, ctrls={93:75, 91:75})
 
-#closer_high = Transpose(0) >> Output('q49', 1, 15, 100)
-#closer_base = Transpose(0) >> Output('q49', 2, 51, 110)
-#closer_main = KeySplit('c3', closer_base, closer_high)
+# Patche pour Closer to the earth
+closer_high = Output('Q49', 1, 15, 100)
+closer_base = Output('Q49', 2, 51, 110)
+closer_main = KeySplit('c3', closer_base, closer_high)
 
 _scenes = {
-    1: Scene("PatchName", patch),
-    2: barchetta,
-    3: freewill
+    2:  Scene("Intro", _intro),
+    1:  Scene("MP3", Filter(CTRL) >> CtrlSplit({ 23: System("mpg123 /mnt/flash/root/online.mp3")})),
+    20: Scene("RedBarchetta", LatchNotes(False,reset='C3') >> keysynth),
+    21: Scene("FreeWill", Transpose(12) >> LatchNotes(False,reset='E4') >> keysynth),
+    22: Scene("CloserToTheHeart", closer_main)
 }
 
 run(
