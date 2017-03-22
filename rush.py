00bebed99d26a3ac372e182cdb62642bfdd45a8f
Stephane Gagnon <admin@pacificweb.ca>
2017-03-21 21:50:45 -0400

    Scene navigation stable

--- rush.py
+++ rush.py
@@ -17,14 +17,25 @@ config(
 
 # Scene navigation
 def NavigateToScene(ev):
-    switch_scene(ev.ctrl)
-
+    nb_scenes = len(scenes())    
+    if ev.ctrl == 20:
+        cs=current_scene()
+        if ev.value == 0:
+            if cs < nb_scenes:
+                switch_scene(cs+1)
+        elif ev.value == 1:
+            if cs > 1:
+                switch_scene(cs-1)
+        elif ev.value == 2:
+            css=current_subscene()
+            switch_subscene(css+1)
+    
 # Pre/Post
 _pre = Print('input', portnames='in')
 _post = Print('output', portnames='out')
 
 # Controller pour le changement de scene
-_control = Filter(CTRL) >> CtrlFilter([20,21,22]) >> Process(NavigateToScene)
+_control = Filter(CTRL) >> CtrlFilter(20) >> Process(NavigateToScene)
 
 _intro = Output('Q49', channel=1, program=22, volume=100)
 
@@ -37,16 +48,20 @@ closer_base = Output('Q49', 2, 51, 110)
 closer_main = KeySplit('c3', closer_base, closer_high)
 
 _scenes = {
-    2:  Scene("Intro", _intro),
-    1:  Scene("MP3", Filter(CTRL) >> CtrlSplit({ 23: System("mpg123 /mnt/flash/root/online.mp3")})),
-    20: Scene("RedBarchetta", LatchNotes(False,reset='C3') >> keysynth),
-    21: Scene("FreeWill", Transpose(12) >> LatchNotes(False,reset='E4') >> keysynth),
-    22: Scene("CloserToTheHeart", closer_main)
+    1: Scene("Intro", _intro),
+    2: Scene("MP3", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 /mnt/flash/root/online.mp3")),
+    3: Scene("RedBarchetta", LatchNotes(False,reset='C3') >> keysynth),
+    4: Scene("FreeWill", Transpose(12) >> LatchNotes(False,reset='E4') >> keysynth),
+    5: Scene("CloserToTheHeart", closer_main),
+    6: SceneGroup("2112", [
+           Scene("Intro",  Filter(CTRL) >> CtrlSplit({ 21: System("mpg123 /mnt/flash/rush/2112.mp3")})),
+           Scene("Explosion",  Filter(CTRL) >> CtrlSplit({ 21: System("mpg123 /mnt/flash/alert.mp3")})),
+       ]),
 }
 
 run(
     control=_control,
-    pre=_pre, 
+#    pre=_pre, 
 #    post=_post,
     scenes=_scenes, 
 )
