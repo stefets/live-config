4656c3fc0e695f197fd251714ae4f772e8392442
Stephane Gagnon <stephane-gagnon@hotmail.com>
2017-03-28 21:34:12 -0400

    TSS upgrade

--- rush.py
+++ rush.py
@@ -30,7 +30,7 @@ def NavigateToScene(ev):
             if cs > 1:
                 switch_scene(cs-1)
         elif ev.value == 2:
-            #TODO - WRAP SUBSCENE
+            #TODO - wrap subscene
             css=current_subscene()
             switch_subscene(css+1)
     
@@ -40,6 +40,9 @@ _post = Print('output', portnames='out')
 
 # Controller pour le changement de scene
 _control = Filter(CTRL) >> CtrlFilter(20) >> Process(NavigateToScene)
+
+# FX
+explosion = Velocity(fixed=100) >> Output('PK5', channel=1, program=((96*128)+3,128), volume=100)
 
 # Patch Synth. generique pour Barchetta, FreeWill, Limelight etc...
 keysynth = Velocity(fixed=80) >> Output('PK5', channel=1, program=((99*128),82), volume=100, ctrls={93:75, 91:75})
@@ -55,14 +58,11 @@ closer_main = KeySplit('c3', closer_base, closer_high)
 # Patch pour Time Stand Still
 tss_high = Velocity(fixed=90) >> Output('Q49', channel=1, program=((99*128),92), volume=100)
 tss_base = Transpose(12) >> Velocity(fixed=90) >> Output('Q49', channel=1, program=((99*128),92), volume=100)
-tss_synth = KeySplit('c2', tss_base, tss_high)
+tss_keyboard = KeySplit('c2', tss_base, tss_high)
 tss_foot = Transpose(-24) >> Velocity(fixed=100) >> Output('PK5', channel=2, program=((99*128),103), volume=100)
 
-# Explosion 2112
-explosion = Velocity(fixed=127) >> Output('PK5', channel=1, program=((96*128)+3,128), volume=120)
-
 # Patch debug
-debug = Output('PK5', channel=1, program=1, volume=100)
+debug = Output('PK5', channel=1, program=((99*128), 1), volume=100)
 
 # Liste des scenes
 _scenes = {
@@ -72,30 +72,22 @@ _scenes = {
     4: Scene("CloserToTheHeart", closer_main),
     5: SceneGroup("The Trees", [
            Scene("Bridge",  Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/trees_full.mp3")),
-           Scene("Synth",  Transpose(-29) >> LatchNotes(False,reset='C3') >> lowsynth),
+           Scene("Synth", Transpose(-29) >> LatchNotes(False,reset='G0') >> lowsynth),
        ]),
     6: SceneGroup("Time Stand Still", [
-           Scene("Init PK5", tss_foot),
-           Scene("Init Q49", tss_synth),
+           Scene("Q49@ch1 & PK5@ch2", [tss_keyboard, tss_foot]),
        ]),
     7: SceneGroup("2112", [
-           Scene("Intro",  Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/2112.mp3")),
+           Scene("Intro", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/2112.mp3")),
            Scene("Explosion", explosion),
        ])
 }
 
-_tmp = {
-    1: Channel(1),
-    2: Channel(1),
-    3: Channel(1),
-    4: Channel(1),
-    5: Channel(1),
-} 
-
-# Main 
+# ---------------------------
 run(
     control=_control,
     pre=_pre, 
-#    post=_post,
-    scenes=_tmp, 
+    #post=_post,
+    scenes=_scenes, 
 )
+# ---------------------------
