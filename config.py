98d9c04e39b05e564a286b2b6539395e60b4ad0f
Stephane Gagnon <stephane-gagnon@hotmail.com>
2017-04-06 19:24:58 -0400

    kill swtich and filters

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import subprocess
 from mididings import *
 from mididings.extra import *
 from mididings.engine import *
@@ -18,10 +19,11 @@ config(
         ('SD90 - MIDI IN 1', '20:2','.*SD-90 MIDI 1'),
         ('SD90 - MIDI IN 2', '20:3','.*SD-90 MIDI 2') ],
 
-    initial_scene = 8,
+    initial_scene = 1,
 )
 
-
+#--------------------------------------------------------------------
+# Test
 def Chord(ev, trigger_notes=(41, 43), chord_offsets=(0, 4, 7)):
     if ev.type in (NOTEON, NOTEOFF):
         if ev.data1 in trigger_notes:
@@ -29,9 +31,12 @@ def Chord(ev, trigger_notes=(41, 43), chord_offsets=(0, 4, 7)):
             return [evcls(ev.port, ev.channel, ev.note + i, ev.velocity)
                     for i in chord_offsets]
     return ev
+#--------------------------------------------------------------------
 
+# For SD-90 only
 def SendSysex(ev):
     return SysExEvent(ev.port, '\xF0\x41\x10\x00\x48\x12\x00\x00\x00\x00\x00\x00\xF7')
+#--------------------------------------------------------------------
 
 # Scene navigation
 def NavigateToScene(ev):
@@ -48,62 +53,77 @@ def NavigateToScene(ev):
             #TODO - wrap subscene
             css=current_subscene()
             switch_subscene(css+1)
-        elif ev.value == 3:
-            restart()    
+    elif ev.ctrl == 22:
+        subprocess.Popen(['/bin/bash', './kill.sh'])
 
 # Pre/Post
 _pre = Print('input', portnames='in')
 _post = Print('output', portnames='out')
+#--------------------------------------------------------------------
 
 # Controller pour le changement de scene
-_control = ChannelFilter(9) >> Filter(CTRL) >> CtrlFilter(20) >> Process(NavigateToScene)
+_control = ChannelFilter(9) >> Filter(CTRL) >> CtrlFilter([20,22]) >> Process(NavigateToScene)
+#--------------------------------------------------------------------
 
-# Play Switch (ToTry)
-#play = Filter(CTRL) >> CtrlFilter(21)
+# Channel filter base for patche
+cf=ChannelFilter(channels=[1,2])
+#--------------------------------------------------------------------
 
-# FX
+# Shortcut
+play = Filter(CTRL) >> CtrlFilter(21)
+#reset = Filter(CTRL) >> CtrlFilter(22) >> Panic()
+#--------------------------------------------------------------------
+
+# FX Section
 explosion = Velocity(fixed=80) >> Output('PK5', channel=1, program=((96*128)+3,128), volume=100)
+#--------------------------------------------------------------------
 
 # Patch Synth. generique pour Barchetta, FreeWill, Limelight etc...
 keysynth = Velocity(fixed=80) >> Output('PK5', channel=1, program=((99*128),82), volume=100, ctrls={93:75, 91:75})
+#--------------------------------------------------------------------
 
 # Patch Syhth. generique pour lowbase
-lowsynth = Velocity(fixed=100) >> Output('PK5', channel=1, program=51, volume=100, ctrls={93:75, 91:75})
+lowsynth = cf >> Velocity(fixed=100) >> Output('PK5', channel=1, program=51, volume=100, ctrls={93:75, 91:75})
+#--------------------------------------------------------------------
 
-# Patch pour Closer to the hearth
-closer_high = Output('Q49', 1, 15, 100)
-closer_base = Output('Q49', 2, 51, 110)
-closer_main = KeySplit('c3', closer_base, closer_high)
+# Patch pour Closer to the hearth 
+closer_high = Output('Q49', 1, program=((99*128),15), volume=100)
+closer_base = Output('PK5', 2, program=((99*128),51), volume=100)
+closer_main = cf >> KeySplit('c3', closer_base, closer_high)
+#--------------------------------------------------------------------
 
 # Patch pour Time Stand Still
 tss_high = Velocity(fixed=90) >> Output('Q49', channel=1, program=((99*128),92), volume=100)
 tss_base = Transpose(12) >> Velocity(fixed=90) >> Output('Q49', channel=1, program=((99*128),92), volume=100)
-tss_keyboard_main = KeySplit('c2', tss_base, tss_high)
+tss_keyboard_main = cf >> KeySplit('c2', tss_base, tss_high)
+
 tss_foot_left = Transpose(-12) >> Velocity(fixed=75) >> Output('PK5', channel=2, program=((99*128),103), volume=75)
 tss_foot_right = Transpose(-24) >> Velocity(fixed=75) >> Output('PK5', channel=2, program=((99*128),103), volume=75)
-tss_foot_main = KeySplit('d#3', tss_foot_left, tss_foot_right)
+tss_foot_main = cf >> KeySplit('d#3', tss_foot_left, tss_foot_right)
+#--------------------------------------------------------------------
 
 # Patch pour Analog Kid
-analogkid=Transpose(-24) >> Harmonize('c', 'major', ['unison', 'third', 'fifth']) >> Output('Q49', channel=1, program=((99*128),49), volume=100)
+analogkid=cf >> Transpose(-24) >> Harmonize('c', 'major', ['unison', 'third', 'fifth', 'octave']) >> Output('PK5', channel=1, program=((99*128),50), volume=100)
+#--------------------------------------------------------------------
 
 # Patch debug
 #debug = (ChannelFilter(1) >> Output('PK5', channel=1, program=((99*128), 1), volume=100)) // (ChannelFilter(2) >> Output('Q49', channel=3, program=((99*128), 10), volume=101))
-
-init=Filter(NOTE) >> Process(SendSysex)
+#piano=Harmonize('c', 'major', ['unison','octave']) >> Output('Q49', channel=1, program=((99*128),1), volume=100)
+piano= Output('Q49', channel=1, program=((99*128),1), volume=100)
+#--------------------------------------------------------------------
 
 # Liste des scenes
+init=Filter(NOTE) >> Filter(NOTEON) >> Process(SendSysex)
 _scenes = {
     1: Scene("Initialize",  init),
     2: Scene("RedBarchetta", LatchNotes(False,reset='C3') >> keysynth),
     3: Scene("FreeWill", Transpose(12) >> LatchNotes(False,reset='E4') >> keysynth),
-    4: Scene("CloserToTheHeart", closer_main),
+    4: Scene("CloserToTheHeart", [ChannelFilter(1) >> closer_main, ChannelFilter(2) >> Transpose(-24) >> closer_base]),
     5: SceneGroup("The Trees", [
-           Scene("Bridge",  Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/trees_full.mp3")),
+           Scene("Bridge",  play >> System("mpg123 -q /mnt/flash/rush/trees_full.mp3")),
            Scene("Synth", Transpose(-29) >> LatchNotes(False,reset='G0') >> lowsynth),
        ]),
-    6: SceneGroup("Time Stand Still", [
-           Scene("Q49@ch1 & PK5@ch2", [ChannelFilter(1) >> tss_keyboard_main, ChannelFilter(2) >> LatchNotes(False, reset='c4') >> tss_foot_main]),
-       ]),
+    6: Scene("Time Stand Still", [ChannelFilter(1) >> tss_keyboard_main, ChannelFilter(2) >> LatchNotes(False, reset='c4') >> tss_foot_main]),
     7: SceneGroup("2112", [
            Scene("Intro", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/2112.mp3")),
            Scene("Explosion", explosion),
@@ -131,7 +151,7 @@ _scenes = {
 run(
     control=_control,
     pre=_pre, 
-    post=_post,
+    #post=_post,
     scenes=_scenes, 
 )
 # ---------------------------
---
+++ kill.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+# Kill mpg123  process
+kill -9 $(pgrep mpg123) 2>/dev/null
