9223348f3085006038223d071605ca542d009f06
Stephane Gagnon <stephane-gagnon@hotmail.com>
2017-04-06 10:52:37 -0400

    Harmonize for analogkid

--- config.py
+++ config.py
@@ -18,23 +18,17 @@ config(
         ('SD90 - MIDI IN 1', '20:2','.*SD-90 MIDI 1'),
         ('SD90 - MIDI IN 2', '20:3','.*SD-90 MIDI 2') ],
 
-    initial_scene = 1,
+    initial_scene = 8,
 )
 
 
-def Chord(ev):
-    if ev.data1 == 65:
-        return [ CtrlEvent(ev.port, ev.channel, 120, 0),
-                 NoteOnEvent(ev.port, ev.channel, ev.data1, ev.data2), 
-                 NoteOnEvent(ev.port, ev.channel, ev.data1+4, ev.data2), 
-                 NoteOnEvent(ev.port, ev.channel, ev.data1+7, ev.data2) ]
-    elif ev.data1 == 67:
-        return [ CtrlEvent(ev.port, ev.channel, 120, 0),
-				 NoteOnEvent(ev.port, ev.channel, ev.data1, ev.data2), 
-                 NoteOnEvent(ev.port, ev.channel, ev.data1+4, ev.data2), 
-                 NoteOnEvent(ev.port, ev.channel, ev.data1+7, ev.data2) ]
-    else:
-        return CtrlEvent(ev.port, ev.channel, 120, 0)
+def Chord(ev, trigger_notes=(41, 43), chord_offsets=(0, 4, 7)):
+    if ev.type in (NOTEON, NOTEOFF):
+        if ev.data1 in trigger_notes:
+            evcls = NoteOnEvent if ev.type == NOTEON else NoteOffEvent
+            return [evcls(ev.port, ev.channel, ev.note + i, ev.velocity)
+                    for i in chord_offsets]
+    return ev
 
 def SendSysex(ev):
     return SysExEvent(ev.port, '\xF0\x41\x10\x00\x48\x12\x00\x00\x00\x00\x00\x00\xF7')
@@ -90,15 +84,16 @@ tss_foot_right = Transpose(-24) >> Velocity(fixed=75) >> Output('PK5', channel=2
 tss_foot_main = KeySplit('d#3', tss_foot_left, tss_foot_right)
 
 # Patch pour Analog Kid
-analogkid_chord=Filter(NOTE) >> Filter(NOTEON) >> Process(Chord) >> Transpose(-24) >> Output('Q49', channel=1, program=((99*128),92), volume=100)
+analogkid=Transpose(-24) >> Harmonize('c', 'major', ['unison', 'third', 'fifth']) >> Output('Q49', channel=1, program=((99*128),49), volume=100)
 
 # Patch debug
 #debug = (ChannelFilter(1) >> Output('PK5', channel=1, program=((99*128), 1), volume=100)) // (ChannelFilter(2) >> Output('Q49', channel=3, program=((99*128), 10), volume=101))
 
+init=Filter(NOTE) >> Process(SendSysex)
 
 # Liste des scenes
 _scenes = {
-    1: Scene("Initialize",  initialize),
+    1: Scene("Initialize",  init),
     2: Scene("RedBarchetta", LatchNotes(False,reset='C3') >> keysynth),
     3: Scene("FreeWill", Transpose(12) >> LatchNotes(False,reset='E4') >> keysynth),
     4: Scene("CloserToTheHeart", closer_main),
@@ -113,7 +108,8 @@ _scenes = {
            Scene("Intro", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/rush/2112.mp3")),
            Scene("Explosion", explosion),
        ]),
-    8: SceneGroup("Bass cover", [
+    8: Scene("AnalogKid", analogkid),
+    9: SceneGroup("Bass cover", [
            Scene("Toto - Rossana", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/solo/audio/toto_rossana_no_bass.mp3")),
            Scene("Toto - Africa", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/solo/audio/toto_africa_no_bass.mp3")),
            Scene("Yes - Owner of a lonely heart", Filter(CTRL) >> CtrlFilter(21) >> System("mpg123 -q /mnt/flash/solo/audio/yes_owner_lonely_heart.mp3")),
