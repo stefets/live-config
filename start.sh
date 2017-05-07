42881c50443a5d38cf18864c7b5f17da77d6709e
Stephane Gagnon <stephane-gagnon@hotmail.com>
2017-05-07 17:06:21 -0400

    Ajustements

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Start the live music system
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Start the live music system
 module='Roland Corp. EDIROL SD-90'
 data='/mnt/flash/live'
 
-#Check & Unmount & ReMount flash
-sh ./unmount.sh &> /dev/null
-sh ./mount.sh > /dev/null
+#Mount flash 
+sh $DIR/unmount.sh
+sh $DIR/mount.sh > /dev/null
 if ! [ -d "$data" ]
 then
 	echo "$data not found"
 	exit 1
 fi
 
-#Check module
+#Check sound module
 if [ $(lsusb | grep -c "$module") -eq 0 ]
 then
 	echo "$module not found"
@@ -22,4 +23,4 @@ then
 fi
 
 # OK
-python ./live.py
+python $DIR/live.py
