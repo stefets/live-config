792a3f47bd07def046811de25978c9495f74956e
stefets <stephane-gagnon@hotmail.com>
2019-01-16 12:40:47 -0500

    EVO

--- start.sh
+++ start.sh
@@ -4,27 +4,33 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Start the live music system
 module='Roland Corp. EDIROL SD-90'
-data='/mnt/flash/music/soundlib'
 
-#Check sound module
+failed=0
+
+#Check MIDI sound module
 if [ $(lsusb | grep -c "$module") -eq 0 ]
 then
 	echo "$module not found"
-	exit 1
+	failed=1
 fi
 
 #Mount the usb flash stick containg audio files
+data='/mnt/flash/music/soundlib'
 sudo mkdir -m777 -p /mnt/flash
 sudo mount /dev/sda1 /mnt/flash
 if  [ ! -d "$data" ]
 then
-	echo "$data not found"
-	exit 1
+	echo "$data not found, force create"
+	mkdir -p $data
+	failed=1
+fi
+
+if  [ $failed -eq 1 ]
+then
+	read -n 1 -s -r -p "WARNING - press any key to continue or ctrl-c to abort"
 fi
 
 # Create a symlink in /tmp with the usb soundlib 
 ln -sf /mnt/flash/music/soundlib /tmp/soundlib
 
-# OK
-cd $DIR
 /bin/bash $DIR/menu.sh
