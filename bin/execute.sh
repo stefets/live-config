b04856d4025894e241e858590261aa07fae83e39
Stephane Gagnon <stephane-gagnon@hotmail.com>
2020-03-15 22:07:04 -0400

    Separer les configs de hardware et de scenes dans leur dossier respectifs

--- bin/execute.sh
+++ bin/execute.sh
@@ -17,16 +17,16 @@ sed \
     -e "/__FUNCTIONS__/r functions.py" \
     -e "/__FILTERS__/r filters.py" \
     -e "/__CONTROL__/r control.py" \
-    -e "/__SOUNDMODULE__/r soundmodule.py" \
-	-e "/__HD500__/r hd500.py" \
-	-e "/__GT10B__/r gt10b.py" \
+    -e "/__SD90__/r ./hardware/sd90.py" \
+	-e "/__HD500__/r ./hardware/hd500.py" \
+	-e "/__GT10B__/r ./hardware/gt10b.py" \
 	-e "/__PATCHES__/r patches.py" \
     -e "/__SCENES__/r $scenes" \
     -e "/__CONTROLLER__/r $ctrl" \
 	-e "/__FUNCTIONS__/d" \
 	-e "/__FILTERS__/d" \
 	-e "/__CONTROL__/d" \
-	-e "/__SOUNDMODULE__/d" \
+	-e "/__SD90__/d" \
 	-e "/__HD500__/d" \
 	-e "/__GT10B__/d" \
 	-e "/__PATCHES__/d" \
