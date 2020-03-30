bbbf5b5d5299a8ef1f80fa928286ac6378317d63
stefets <stephane-gagnon@hotmail.com>
2020-03-30 18:11:37 -0400

    Reduce bash dependencies
    Remove fcb1010 and keyboard bash script, move into menu for the moment

--- src/menu.sh
+++ src/menu.sh
@@ -4,17 +4,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function process()
 { 
+    OUT="$(mktemp)"
 	case $1 in
 		0 )
             $DIR/keyboard.sh
 			break
 			;;
 		1 )	
+            echo "_ctrl=fcb1010" > $OUT
             items=(palindrome push)
             while true
             do
                 choice=$(dialog --begin 0 0 --no-shadow --output-fd 1 --menu "Configuration" 20 20 10 0 ${items[0]} 1 ${items[1]})
-                $DIR/fcb1010.sh ${items[$choice]}.py
+                $DIR/execute.sh ${items[$choice]}.py $OUT
             done
 			break
 			;;
