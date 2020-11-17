1cd98c12a6e2aaa3b5998de3f74cdbac9cec179f
Stephane Gagnon <stephane-gagnon@hotmail.com>
2020-11-16 19:51:29 -0500

    Scene optimization

--- src/scenes/rush_cover.py
+++ src/scenes/rush_cover.py
@@ -1,10 +1,4 @@
     2: SceneGroup("Rush cover", [
-        Scene("Mission", play >> System(play_file("mission.mp3"))),
-        Scene("Limelight", play >> System(play_file("limelight.mp3"))),
-        Scene("RedBarchetta", play >> System(play_file("barchetta.mp3"))),
-        Scene("FlyByNight", play >> System(play_file("fly_by_night.mp3"))),
-        Scene("Spirit of Radio", play >> System(play_file("spirit_of_radio.mp3"))),
-        Scene("AnalogKid", play >> System(play_file("analogkid.mp3"))),
         Scene("Analog Kid Keyboard", analogkid_main),
         #Scene("Analog Kid Keyboard", [ChannelFilter(2) >> analogkid_main, ChannelFilter(1) >> analogkid_ending ]),
         Scene("TimeStandSteel", play >> System(play_file("time_stand_steel.mp3"))),
