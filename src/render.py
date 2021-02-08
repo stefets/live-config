6312c8c9d27d304590bb8854a8c06d070724345a
stefets <stephane-gagnon@hotmail.com>
2021-02-07 19:51:10 -0500

    Refact marathon patch

--- src/render.py
+++ src/render.py
@@ -204,8 +204,10 @@ def OnPitchbend(ev, direction):
 # filters.py
 #-----------------------------------------------------------------------------------------------------------
 # # ALLOWED FILTERS : Available for patches, meaning, allow only for instance
-q49 = ChannelFilter(1)  # Filter by hardware / channel
-pk5 = ChannelFilter(2)  # Filter by hardware & channel
+keyboard_channel=1
+pk5_channel=3
+q49 = ChannelFilter(keyboard_channel)  # Filter by hardware / channel
+pk5 = ChannelFilter(pk5_channel)  # Filter by hardware & channel
 # fcb=ChannelFilter(9)
 # hd500=ChannelFilter(9)
 # # gt10b=ChannelFilter(16)
@@ -1226,31 +1228,38 @@ big_country_harmonizer = ((
 #-----------------------------------------------------------------------------------------------------------
 _scenes = {
     1: Scene("Initialize", init_patch=InitSoundModule, patch=Discard()),
-    2: SceneGroup("solo-mode",
+    2: SceneGroup("Rush",
         [
-            Scene("Rush", init_patch=P02A, patch=Discard()),
-            Scene("BigCountry", init_patch=P14A, patch=Discard()),
-        ]),
-    3: SceneGroup("styx",
+        Scene("Default", init_patch=P02A, patch=Discard()),
+        Scene("Analog Kid Keyboard", analogkid_main),
+        #Scene("Analog Kid Keyboard", [ChannelFilter(2) >> analogkid_main, ChannelFilter(1) >> analogkid_ending ]),
+        Scene("Time Stand Still Keyboard", [ChannelFilter(1) >> tss_keyboard_main, ChannelFilter(2) >> LatchNotes(False, reset='c4') >> tss_foot_main]),
+        Scene("KidGloves Keyboard", Transpose(0) >> LatchNotes(False,reset='F3')  >> Harmonize('c', 'major', ['unison', 'octave']) >> keysynth),
+        Scene("FreeWill Keyboard", Transpose(0) >> LatchNotes(False,reset='E3')  >> Harmonize('c', 'major', ['unison', 'octave']) >> keysynth),
+       ]),
+    3: SceneGroup("Closer",
         [
-            Scene("Default", init_patch=U01_A, patch=Discard()),
+            Scene("Default", init_patch=Discard(), patch=closer_main),
         ]),
-    4: SceneGroup("tabarnac",
-        [
-            Scene("Default", patch=Discard()),
-        ]),
-    5: SceneGroup("palindrome",
-        [
-            Scene("Centurion - guitar/synth cover", patch=centurion_patch),
-        ]),
-    6: SceneGroup("rush_cover",
-        [
-            Scene("Default", init_patch=P02A, patch=Discard()),
-        ]),
-    7: SceneGroup("bass_cover",
-        [
-            Scene("Default", init_patch=Discard(), patch=Discard()),
-        ]),
+    4:SceneGroup ("Marathon", [
+        Scene("Marathon-Intro/Chords", Port(1) >> (
+          [
+            ChannelSplit({
+                keyboard_channel : marathon_intro,
+                pk5_channel : marathon_chords,
+            }),
+            (ChannelFilter(9) >> Filter(CTRL) >> CtrlFilter(1,2) >> Port(1) >> Fork([Channel(3),Channel(4)]) >>
+            [
+             (CtrlFilter(2)>>Process(OnPitchbend,direction=-1)) //
+                (CtrlFilter(1)>>CtrlMap(1,7))
+            ])
+          ])),
+        Scene("Marathon-Bridge/Solo/Ending", 
+            ChannelSplit({
+                1 : (marathon_bridge // marathon_bridge_split),
+                2 : marathon_chords,
+            })),
+   ]),
 }
 #-----------------------------------------------------------------------------------------------------------
 
