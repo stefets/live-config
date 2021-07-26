e5b633cfac6424ad8be90ff840ac0842c50a276c
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2021-07-25 20:56:57 -0400

    Change venv path

--- src/load.sh
+++ src/load.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Environment
-source $DIR/../.venv/bin/activate
+source ~/.venv/mididings/bin/activate
 
 app="$DIR/build.sh"
 chmod +x $app
