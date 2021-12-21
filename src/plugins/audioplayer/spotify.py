b546171887d6fcd7dd15f57bdfbbf3d436ca1d6a
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2021-12-21 07:52:14 -0500

    Skel

---
+++ src/plugins/audioplayer/spotify.py
@@ -0,0 +1,9 @@
+'''
+    This plugin plays Spotify stream with Spotipy
+'''
+class SpotifyPlayer(object):
+    def __init__(self, config, credentials) -> None:
+        pass
+
+    def __call__(self, ev):
+        pass
