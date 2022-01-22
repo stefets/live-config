6b90c04a7b7bfab37700d3ac4ab5d427331907f8
Stephane Gagnon <stefets>
2022-01-22 08:23:08 -0500

    Test file

---
+++ src/plugins/audioplayer/test_spotify.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
