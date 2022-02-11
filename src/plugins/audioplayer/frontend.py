07e7b83a9c4956e774458d28440a851e9044fc01
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2022-02-10 20:57:12 -0500

    Fix syntax

--- src/plugins/audioplayer/frontend.py
+++ src/plugins/audioplayer/frontend.py
@@ -6,9 +6,9 @@ class Terminal():
         self.spacer = " " * 80
 
     def write_line(self, text) -> None:
-        self.clear_sreen()
+        self.clear_screen()
         print("{}{}{}{}".format(Style.BRIGHT, Fore.GREEN, text, Style.RESET_ALL))
 
     def write_line2(self, text1, text2) -> None:
-        print("{}{}{} {}{}{}{}".format(Style.BRIGHT, Fore.YELLOW,text1), Style.RESET_ALL, Style.BRIGHT, Fore.WHITE, text2, Style.RESET_ALL)
+        print("{}{}{} {}{}{}{}".format(Style.BRIGHT, Fore.YELLOW,text1, Style.RESET_ALL, Style.BRIGHT, Fore.WHITE, text2, Style.RESET_ALL))
 
