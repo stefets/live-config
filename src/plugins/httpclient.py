1347e699aee24e4260dd6a5ed4d63c9f615d6050
Stephane Gagnon <stephane-gagnon@hotmail.com>
2023-06-13 20:29:05 -0400

    #105 - WIP Http plugin refact

--- src/plugins/httpclient.py
+++ src/plugins/httpclient.py
@@ -2,57 +2,71 @@ import requests
 from requests.exceptions import ConnectionError
 
 '''
-    Plugin qui envoie un request via Call
+    Plugin qui envoie un Http via Call
     Un remplacement est possible dans l'uri en utilisant data1 et data2
     Ex : 
     -   F"/api/scenes/{}/subscenes/{}"
     -   /api/scenes/ev.data1/subscenes/ev.data2
 '''
 
-class RequestBase():
+''' Abstract '''
+class HttpClientBase():
     def __init__(self, config, uri):
         self.enable = config["enable"]
         self.uri = uri
 
     def get(self, ev):
+        if not self.enable:
+            return
+
         try:
             r = requests.get(self.uri.format(ev.data1, ev.data2))
-            if r.status_code not in [200, 204]:
+            if r.status_code not in [200, 300]:
                 print(r.status_code) 
         except Exception:
             print(500)
 
-    def post(self):
-        print(501)
+    def post(self, ev):
+        if not self.enable:
+            print(501)
 
-    def delete(self):
-        print(501)
+    def delete(self, ev):
+        if not self.enable:
+            print(501)
 
-    def put(self):
-        print(501)
+    def put(self, ev):
+        if not self.enable:
+            print(501)
 
-''' GET '''
-class RequestGet(RequestBase):
+
+''' 
+    Childs 
+'''
+class HttpGet(HttpClientBase):
     def __init__(self, config, uri):
         super().__init__(config, uri)
 
     def __call__(self, ev):
-        if self.enable:
-            super().get(ev)
+        super().get(ev)
 
-class RequestPost(RequestBase):
+class HttpPost(HttpClientBase):
     def __init__(self, config, uri):
         super().__init__(config, uri)
 
     def __call__(self, ev):
-        if self.enable:
-            super().post()
+        super().post(ev)
 
-class RequestDelete(RequestBase):
+class HttpPut(HttpClientBase):
     def __init__(self, config, uri):
         super().__init__(config, uri)
 
     def __call__(self, ev):
-        if self.enable:
-            super().delete()
+        super().put(ev)
+
+class HttpDelete(HttpClientBase):
+    def __init__(self, config, uri):
+        super().__init__(config, uri)
+
+    def __call__(self, ev):
+        super().delete(ev)
 
