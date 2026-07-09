b27cf8ef38ecd578367f1db832c730335c103dad
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-07-08 22:33:57 -0400

    chore: Add file in hook

--- src/includes/functions/hook.py
+++ src/includes/functions/hook.py
@@ -3,6 +3,7 @@ hook(
     OSCInterface(),
     MemorizeScene("/tmp/hook.memorize_scene"),
     AutoRestart(filenames=["includes/scenes.py",
+                           "includes/controls.py",
                            "includes/functions/hook.py",
                            "includes/functions/run.py"])
 )
