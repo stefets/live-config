cd6f1c6bd7a242ff1ce2cb75c8cd7d78c193a90c
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-08-05 22:59:19 -0400

    refactor: Add Party Mix V3 and restaure Party Mix V2 config

--- src/tests/in_out_custom.py
+++ src/tests/in_out_custom.py
@@ -61,7 +61,8 @@ config(
         (gt1000_midi_1,'.*GT-1000 MIDI 1.*',),
         (gt1000_midi_2,'.*GT-1000 MIDI 2.*',),
         (mixxx_midi_0,'.*VirMIDI.*-0$',),
-        (numark_midi_0,'.*Party Mix MKII MIDI 1.*',),
+        (numark_midi_pmv3_0,'.*Party Mix III MIDI 1.*',),
+        (numark_midi_pmv2_0,'.*Party Mix MKII MIDI 1.*',),
         (um2_midi_1,'.*UM-2 MIDI 1.*',),
         (um2_midi_2,'.*UM-2 MIDI 2.*',),
     ],
@@ -80,7 +81,8 @@ config(
         (gt1000_midi_1,'.*GT-1000 MIDI 1.*',),
         (gt1000_midi_2,'.*GT-1000 MIDI 2.*',),
         (mixxx_midi_0,'.*VirMIDI.*-0$',),
-        (numark_midi_0,'.*Party Mix MKII MIDI 1.*',),
+        (numark_midi_pmv3_0,'.*Party Mix III MIDI 1.*',),
+        (numark_midi_pmv2_0,'.*Party Mix MKII MIDI 1.*',),
         (um2_midi_1,'.*UM-2 MIDI 1.*',),
     ],
 )
@@ -101,7 +103,7 @@ post = Print('output',portnames='out')
 run(
     control=Pass(),
     scenes = {
-        1 : Scene("Empty", init_patch = =Discard(), patch =Discard()),
+        1 : Scene("Empty", init_patch=Discard(), patch=Discard()),
     },
     pre=pre,
     post=post,
