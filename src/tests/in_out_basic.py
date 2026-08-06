8f989f8ffd42f7dfd4d4caf2d6ba9846dd223c4b
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-08-05 22:57:22 -0400

    feat: Generic test file with VirMIDI

--- src/tests/in_out_basic.py
+++ src/tests/in_out_basic.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-import mididings
+
+from mididings import engine
+from mididings.extra import *
+from mididings.extra.osc import *
+from mididings.extra.inotify import *
+
 
 config(
     backend = 'alsa',
@@ -9,14 +14,33 @@ config(
     client_name = 'mididings',
 
     in_ports = [
-    ],
+        ("vm_0",'.*VirMIDI.*-0$',),
+        ],
 
     out_ports = [
-    ],
+        ("vm_0",'.*VirMIDI.*-0$',),
+        ],
 )
 
+hook(
+    OSCInterface(),
+)
+
+_scenes = {
+    1: SceneGroup("Scene 1", 
+        [
+            Scene("SubScene 1", init_patch=Discard(), patch=Discard()),
+            Scene("SubScene 2", init_patch=Discard(), patch=Discard())
+        ]),
+    2: SceneGroup("Scene 2", 
+        [
+            Scene("SubScene 1", init_patch=Discard(), patch=Discard()),
+            Scene("SubScene 2", init_patch=Discard(), patch=Discard())
+        ]),
+}
+
 run(
-    scenes = {1:Pass()},
+    scenes = _scenes,
     control = Pass(),
     pre  = Print('input  (pre )', portnames='in'),
     post = Print('output (post)', portnames='out'),
