db4d990b48c1b0704a52b822d09162c91d3bd32b
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-08-06 20:22:15 -0400

    refactor: Remove mpg123 files and import #136

--- src/includes/imports.py
+++ src/includes/imports.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Extensions
-from extensions.mp3 import *
+from extensions.mpv import *
 from extensions.vlc import *
 from extensions.philips import *
 from extensions.spotify import *
