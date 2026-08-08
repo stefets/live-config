0403d99532d8689c1a4ee31f2f4a71b2f9d8e7ba
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-08-08 12:43:41 -0400

    chore: Add switches to mpv start command (logging, terminal)

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mpv --idle --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 &
-mpv --idle --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 &
-mpv --idle --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k &
+rm -f /tmp/sd90a.sock /tmp/sd90b.sock /tmp/u192k.sock
+rm -f /tmp/mvp-sd90a.log /tmp/mvp-sd90b.log /tmp/mvp-u192k.log
+
+mpv --idle=yes --keep-open=yes --no-video --no-terminal --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 --log-file=/tmp/mvp-sd90a.log --msg-level=all=info  &
+mpv --idle=yes --keep-open=yes --no-video --no-terminal --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 --log-file=/tmp/mvp-sd90b.log --msg-level=all=info  &
+mpv --idle=yes --keep-open=yes --no-video --no-terminal --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k --log-file=/tmp/mvp-u192k.log --msg-level=all=info &
