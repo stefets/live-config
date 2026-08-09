7cb56e3bfe4d2cb8fd627e5652835fff3c061ccf
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-08-09 18:00:22 -0400

    refactor: Enable playlist listing

--- src/plugins/playlist.py
+++ src/plugins/playlist.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from mididings.engine import scenes, current_scene, current_subscene
-
+from extensions.common import (
+    Terminal
+)
 class PlaylistManager:
     def __init__(self):
         self.playlist = Playlist()
@@ -12,6 +14,7 @@ class PlaylistManager:
         subscene = self.get_subscene_name()
         path = f"{scene}/{subscene}" if subscene else scene
         self.playlist.create(f"/media/soundlib/{path}")
+        self.playlist.listing()
 
     def get_scene_name(self):
         return scenes()[current_scene()][0]
@@ -26,7 +29,7 @@ class PlaylistManager:
 class Playlist:
     def __init__(self):
         self.songs = []
-        # self.terminal = Terminal()
+        self.terminal = Terminal()
 
     def create(self, path):
         self.songs = [
@@ -34,17 +37,16 @@ class Playlist:
             for p in sorted(Path(path).glob("**/*"))
             if p.suffix.lower() in {".mp3"}
         ]
-        if self.songs:
-            pass
-        else:
-            print("No files found in " + path)
-
+  
     def len(self):
         return len(self.songs)
 
-    # def listing(self):
-    #     self.terminal.write_line(self.get_scene_name())
-    #     rank = 0
-    #     for song in self.songs:
-    #         rank += 1
-    #         self.terminal.write_line2(str(rank).zfill(2), song)
+    def listing(self):
+        if not self.songs:
+            self.terminal.write_line("No files found in " + path)
+            return
+
+        rank = 0
+        for song in self.songs:
+            rank += 1
+            self.terminal.write_line2(str(rank).zfill(2), song)
