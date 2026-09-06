9d8048411e25da4f690c7c19e67b8ee75724bd59
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-09-05 20:02:19 -0400

    refactor: Enhance command handling and response reading in MpvClient

--- src/plugins/mpv.py
+++ src/plugins/mpv.py
@@ -13,32 +13,55 @@ class MpvClient():
         
         self.socket = socket.socket(socket.AF_UNIX)
         self.socket.connect(address)
+        
+        self.request_id = 0
+        self._buffer = b""
 
     def command(self, *args):
+        self.request_id += 1
         payload = {
-            "command": list(args)
+            "command": list(args),
+            "request_id": self.request_id
         }
 
         self.socket.sendall(
             (json.dumps(payload) + "\n").encode("utf-8")
         )
+
+        return self._read_response(self.request_id)
         
     def set_property(self, property_name, value):
         self.command("set_property", property_name, value)
         
     def get_property(self, property_name):
-        payload = {
-            "command": ["get_property", property_name]
-        }
+        response = self.command("get_property", property_name)
+        return response.get("data")
 
-        self.socket.sendall(
-            (json.dumps(payload) + "\n").encode("utf-8")
-        )
+    def _read_response(self, request_id):
+        while True:
 
-        response = self.socket.recv(1024)
-        response_data = json.loads(response.decode("utf-8"))
-        return response_data.get("data")
+            # On a peut-être déjà plusieurs messages dans le buffer
+            while b"\n" in self._buffer:
+                line, self._buffer = self._buffer.split(b"\n", 1)
 
+                if not line:
+                    continue
+
+                message = json.loads(line.decode("utf-8"))
+
+                print("MPV MESSAGE:", message)
+
+                if message.get("request_id") == request_id:
+                    return message
+
+            # Pas encore trouvé notre réponse : on lit davantage
+            chunk = self.socket.recv(4096)
+
+            if not chunk:
+                raise ConnectionError("MPV IPC socket closed")
+
+            self._buffer += chunk
+            
     def load(self, filename):
         self.command("loadfile", filename)
 
@@ -63,5 +86,5 @@ class MpvClient():
     def seek(self, offset):
         self.command("seek", offset, "relative")
 
-    def set_volume(self, value):
+    def volume(self, value):
         self.set_property("volume", value)
