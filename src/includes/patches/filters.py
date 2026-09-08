24fa055e2e88d76b9a3123e2910c9b6b69476356
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-09-08 19:56:25 -0400

    refactor(pk5): Add Port

--- src/includes/patches/filters.py
+++ src/includes/patches/filters.py
@@ -5,6 +5,6 @@
 
 mpk_a_filter = [PortFilter(mpk249_port_a), PortFilter(mpk261_port_a)]
 mpk_b_filter = [PortFilter(mpk249_port_b), PortFilter(mpk261_port_b)]
-pk5_filter   = PortFilter(mpk249_midi) >> ChannelFilter(3)
+pk5_filter   = [PortFilter(mpk249_midi), PortFilter(mpk261_midi)] >> ChannelFilter(3)
 
 # -------------------------------
