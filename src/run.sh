c0bcd3ab1c40224e8c6c36a273abfb64aced4e59
Stephane Gagnon <stephane-gagnon@hotmail.com>
2026-09-09 18:44:25 -0400

    fix: Correct log file names in run.sh for mpv instances

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -f /tmp/sd90a.sock /tmp/sd90b.sock /tmp/u192k.sock
-rm -f /tmp/mvp-sd90a.log /tmp/mvp-sd90b.log /tmp/mvp-u192k.log
+rm -f /tmp/mpv-sd90a.log /tmp/mpv-sd90b.log /tmp/mpv-u192k.log
 
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 --log-file=/tmp/mvp-sd90a.log --msg-level=all=info  &
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 --log-file=/tmp/mvp-sd90b.log --msg-level=all=info  &
-mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k --log-file=/tmp/mvp-u192k.log --msg-level=all=info &
+mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90a.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90a.log --msg-level=all=info  &
+mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/sd90b.sock --audio-device=alsa/SD90 --log-file=/tmp/mpv-sd90b.log --msg-level=all=info  &
+mpv --idle=yes --keep-open=no --no-video --no-terminal --input-ipc-server=/tmp/u192k.sock --audio-device=alsa/U192k --log-file=/tmp/mpv-u192k.log --msg-level=all=info &
